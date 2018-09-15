8dd4cc2f3b49838649b54a9ad3a7514e5dcb7112
Caique Lins <caique.c.lins@gmail.com>
2018-09-15 15:32:08 -0300

    Site - Produtos

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 require_once("vendor/autoload.php");
 
+
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
@@ -12,6 +13,7 @@ $app = new Slim();
 
 $app->config('debug', true);
 
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
